899bebb1c67f9104b985c71b931eece7dd70f472
n0rmh3ll <89399773+n0rmh3ll@users.noreply.github.com>
2025-01-14 12:57:44 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-8">Google Authentication Testing</h1>
+      <h1 className="text-3xl font-bold mb-8">Google Authentication </h1>
       <button
         onClick={handleGoogleSignIn}
         className="bg-white text-black font-bold py-2 px-4 rounded-full border-2 border-black hover:bg-black hover:text-white transition-all duration-300 ease-in-out flex items-center"
